1845afc7c5ec48c8e9b1c625ab8a2fcdb36b9aa7
Swaliha Mulla <swaliharoonmulla192@gmail.com>
2025-11-29 14:15:37 +0000

    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="non

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,9 @@ import { Paintbrush, Phone, Instagram, Youtube, Facebook, Linkedin } from 'lucid
 import Link from 'next/link';
 
 const WhatsAppIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5">
-        <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+      <path d="M6.5 17.5 5 22l4.7-1.5a9 9 0 1 0-3.2-3z"/>
+      <path d="M8.5 9.5c0 3 3 6 5.5 6.5.6.1 1-.4 1.3-.8.2-.3.3-.6.6-.5.4.1 1.8.9 2.1 1.1.3.2.3.4.3.7a2.7 2.7 0 0 1-.1.6c-.2.6-1.2 1.3-2 1.4-1 .1-4-.3-6.7-3s-3-5.8-3-6.8c0-.8.8-1.7 1.4-1.9.2-.1.4-.1.6-.1.3 0 .5 0 .7.4.2.4.9 2.2.9 2.2.1.2.1.4 0 .6-.2.3-.2.3-.4.6-.1.2-.2.3-.1.5z"/>
     </svg>
 );
 
